5715d7f3a65059e044548c7daa027cdd561e446a
Luca Olivieri <gliolivieri@live.it>
2018-03-28 21:25:08 +0200

    Node 1 online

--- logger.ino
+++ logger.ino
@@ -9,11 +9,13 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <DHT.h>
+#include "credentials.h"
+
 #define ID 1
 #define DHTTYPE DHT11
+#define INTERVAL 60000
+//#define DEBUG
 
-const char* ssid     = "Soffitta";
-const char* password = "maBelin!";
 const char* url = "http://192.168.1.44/logger/Insert.php";
 
 //DHT dht;
@@ -21,15 +23,14 @@ DHT dht(2, DHTTYPE);
 float temperature, humidity;
 
 void setup() {
+	#ifdef DEBUG
     Serial.begin(115200);
     delay(10);
-
-    // We start by connecting to a WiFi network
-
     Serial.println();
     Serial.println();
     Serial.print("Connecting to ");
     Serial.println(ssid);
+	#endif
 
     /*  Explicitly set the ESP8266 to be a WiFi-client, otherwise, it by default,
         would try to act as both a client and an access-point and could cause
@@ -39,13 +40,14 @@ void setup() {
 
     while (WiFi.status() != WL_CONNECTED) {
         delay(500);
-        Serial.print(".");
     }
 
+	#ifdef DEBUG
     Serial.println("");
     Serial.println("WiFi connected");
     Serial.println("IP address: ");
     Serial.println(WiFi.localIP());
+	#endif
 
     dht.begin();
 }
@@ -53,26 +55,30 @@ void setup() {
 
 
 void loop() {
-	
-	temperature = dht.readTemperature();
-	humidity = dht.readHumidity();
-	
+    temperature = dht.readTemperature();
+    humidity = dht.readHumidity();
+
     if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
 
         HTTPClient http;  //Declare an object of class HTTPClient
         String data = "temp=" + String(temperature) + "&hum=" + String(humidity) + "&id=" + String(ID);
-
+		#ifdef DEBUG
+        Serial.println(data);
+		#endif
         http.begin(url);  //Specify request destination
         http.addHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
         int httpCode = http.POST(data);         //Send the request
 
         if (httpCode > 0) { //Check the returning code
             String payload = http.getString();   //Get the request response payload
-            Serial.println(payload);                     //Print the response payload
+			#ifdef DEBUG
+            Serial.println(payload);             //Print the response payload
+			#endif
         }
         http.end();   //Close connection
     }
     
-    delay(10000);
+    delay(INTERVAL);
 }
+
 
